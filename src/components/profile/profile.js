7e65e6a95d9d8b22f040c1e1d15c095b2c3d732f
Ryan Casey <rcaseyx@gmail.com>
2018-12-09 23:55:54 -0500

    updated profile logic

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 import './profile.css';
 
 import PreferencesForm from '../preferences-form/preferences-form';
@@ -7,26 +8,41 @@ import SavedDog from '../saved-dog/saved-dog';
 
 export class Profile extends React.Component {
     render() {
-      const dogs = this.props.savedDogs.map((dog, index) =>
-          <div key={index}>
-              <SavedDog {...dog} />
-          </div>
-      );
-      return (
-        <div className="profile">
-          <h3>{this.props.user.firstName}'s Profile</h3>
-          <div className="saved-dogs-container">
-            <h4>Saved Dogs</h4>
-            <div className="saved-dogs">
-              {dogs}
+      if (this.props.savedDogs.length > 0) {
+        const dogs = this.props.savedDogs.map((dog, index) =>
+            <div key={index}>
+                <SavedDog {...dog} />
+            </div>
+        );
+        return (
+          <div className="profile">
+            <h3>{this.props.user.firstName}'s Profile</h3>
+            <div className="saved-dogs-container">
+              <h4>Saved Dogs</h4>
+              <div className="saved-dogs">
+                {dogs}
+              </div>
+            </div>
+            <div className="preferences-container">
+              <h4>Your Preferences</h4>
+              <PreferencesForm age={this.props.preferences.age} size={this.props.preferences.size} />
             </div>
           </div>
-          <div className="preferences-container">
-            <h4>Your Preferences</h4>
-            <PreferencesForm age={this.props.preferences.age} size={this.props.preferences.size} />
+        )
+      } else {
+        return (
+          <div className="profile">
+            <h3>{this.props.user.firstName}'s Profile</h3>
+            <div className="saved-dogs-container">
+              Visit <Link to="/search">Find a Friend</Link> to get started!
+            </div>
+            <div className="preferences-container">
+              <h4>Your Preferences</h4>
+              <PreferencesForm age={this.props.preferences.age} size={this.props.preferences.size} />
+            </div>
           </div>
-        </div>
-      )
+        )
+      }
     }
 }
 
