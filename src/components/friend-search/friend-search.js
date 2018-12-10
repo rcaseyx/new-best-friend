e7b363944c9f088d79f75ab466e94ba583678d2c
Ryan Casey <rcaseyx@gmail.com>
2018-12-09 19:30:54 -0500

    updated search

--- src/components/friend-search/friend-search.js
+++ src/components/friend-search/friend-search.js
@@ -33,35 +33,61 @@ const dogs = [
   }
 ];
 
-const dogCards = dogs.map((dog, index) =>
-    <DogCard {...dog} />
-);
-
 export default class FriendSearch extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        i: 0
+      }
+      this.onLikeClick = this.onLikeClick.bind(this);
+      this.onNextClick = this.onNextClick.bind(this);
+  }
+
+    onLikeClick() {
+      const i = this.state.i + 1;
+      console.log(i);
+      this.setState({i});
+      //send dog to savedDogs
+    }
+
+    onNextClick() {
+      const i = this.state.i + 1;
+      console.log(i);
+      this.setState({i});
+    }
+
     render() {
-      return(
-        <div className="search-container">
-            <h4>Find a new best friend!</h4>
-            <div className="cards">
-                {dogCards[0]}
-            </div>
-            <div className="buttons">
-                <input
-                  type="image"
-                  src="https://cdn2.iconfinder.com/data/icons/greenline/512/check-512.png"
-                  alt="Like button"
-                  className="like"
-                  onClick={(e) => e.preventDefault}
-                />
-                <input
-                  type="image"
-                  src="https://cdn3.iconfinder.com/data/icons/audio-sound-and-video/64/audio-video-outline-forward-512.png"
-                  alt="Next button"
-                  className="next"
-                  onClick={(e) => e.preventDefault}
-                />
-            </div>
-        </div>
-      );
+      if (this.state.i < dogs.length) {
+        return (
+          <div className="search-container">
+              <h4>Find a new best friend!</h4>
+              <div className="cards">
+                <DogCard {...dogs[this.state.i]} />
+              </div>
+              <div className="buttons">
+                  <input
+                    type="image"
+                    src="https://cdn2.iconfinder.com/data/icons/greenline/512/check-512.png"
+                    alt="Like button"
+                    className="like"
+                    onClick={this.onLikeClick}
+                  />
+                  <input
+                    type="image"
+                    src="https://cdn3.iconfinder.com/data/icons/audio-sound-and-video/64/audio-video-outline-forward-512.png"
+                    alt="Next button"
+                    className="next"
+                    onClick={this.onNextClick}
+                  />
+              </div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="search-container">
+              <h4>Check back later for more friends!</h4>
+          </div>
+        );
+      }
     }
 }
