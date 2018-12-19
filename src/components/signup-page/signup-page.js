6807d1a77bdb82df1ba103336e1f7e1fd6bdf5c2
Ryan Casey <rcaseyx@gmail.com>
2018-12-19 14:03:48 -0500

    new styles and spinner

--- src/components/signup-page/signup-page.js
+++ src/components/signup-page/signup-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
+import Loader from 'react-loader-spinner';
 
 import SignupForm from '../signup-form/signup-form';
 import './signup-page.css';
@@ -8,6 +9,18 @@ import './signup-page.css';
 export function SignupPage(props) {
     if (props.loggedIn) {
         return <Redirect to="/profile" />;
+    }
+    if (props.loading) {
+        return (
+            <div className="signup-page">
+                <Loader
+                  type="RevolvingDot"
+                  color="#D0DCE0"
+                  height="100"
+                  width="100"
+                />
+            </div>
+        )
     }
     return (
         <div className="signup-page">
@@ -18,7 +31,8 @@ export function SignupPage(props) {
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+    loggedIn: state.auth.currentUser !== null,
+    loading: state.auth.loading
 });
 
 export default connect(mapStateToProps)(SignupPage);
